4ff7b489b6dd2c489059913d18c7b18c8b80bf1c
Kamil Ozcan <kamilozcann@gmail.com>
2023-11-29 12:05:14 -0500

    Modified button list buttons

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -11,6 +11,17 @@ export const ToDoList = ({ toDo, setToDo }) => {
             className="list"
             onChange={(event) => event.preventDefault()}
           />
+          <div>
+            <button className="button-complete task-button">
+              <i className="fa fa-check-circle"></i>
+            </button>
+            <button className="button-edit task-button">
+              <i className="fa fa-edit"></i>
+            </button>
+            <button className="button-delete task-button">
+              <i className="fa fa-trash"></i>
+            </button>
+          </div>
         </li>
       ))}
     </div>
