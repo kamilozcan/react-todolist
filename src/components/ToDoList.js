22098f7363ac88307f13f0ed8a451e9b1fcfbee1
Kamil Ozcan <kamilozcann@gmail.com>
2023-11-29 13:44:07 -0500

    Modified Complete Button (functional)

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,21 +1,35 @@
 import React from "react";
 
 export const ToDoList = ({ toDo, setToDo }) => {
+  const handleComplete = (todo) => {
+    setToDo(
+      toDo.map((item) => {
+        if (item.id === todo.id) {
+          return { ...item, completed: !item.completed };
+        }
+        return item;
+      })
+    );
+  };
+
   const handleDelete = ({ id }) => {
-    setToDo(toDo.filter((toDo) => toDo.id !== id));
+    setToDo(toDo.filter((todo) => todo.id !== id));
   };
   return (
     <div>
-      {toDo.map((toDo) => (
-        <li className="todo-list" key={toDo.id}>
+      {toDo.map((todo) => (
+        <li className="todo-list" key={todo.id}>
           <input
             type="text"
-            value={toDo.title}
+            value={todo.title}
             className="list"
             onChange={(event) => event.preventDefault()}
           />
           <div>
-            <button className="button-complete task-button">
+            <button
+              className="button-complete task-button"
+              onClick={() => handleComplete(todo)}
+            >
               <i className="fa fa-check-circle"></i>
             </button>
             <button className="button-edit task-button">
@@ -23,7 +37,7 @@ export const ToDoList = ({ toDo, setToDo }) => {
             </button>
             <button
               className="button-delete task-button"
-              onClick={() => handleDelete(toDo)}
+              onClick={() => handleDelete(todo)}
             >
               <i className="fa fa-trash"></i>
             </button>
